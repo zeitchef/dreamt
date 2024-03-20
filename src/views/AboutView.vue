48fbcad58b93559986d0d09d58b37cc00cb291e5
Scott Voyles <zeitchef@gmail.com>
2024-03-20 15:41:28 +0100

    feat: fetch eurojackpot numbers

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
-// import { format } from '@formkit/tempo'
+import { formatDate } from '@/utils/date'
+import { computed } from 'vue';
+
+const formattedDate = formatDate(new Date())
 
 const query = gql(`
   query getDraw ($date: String!, $limit: Int!, $type: String!) {
     draw (date: $date, limit: $limit, type: $type) {
-      backendError
+      draws {
+        numbers 
+      }
     }
   }
 `)
-const variables = {
-  date: 'Jan 18 2018',
+
+const { result, loading, error } = useQuery(query, {
+  date: formattedDate,
   limit: 3,
   type: 'eurojackpot'
-}
+})
 
-const { result } = useQuery(query, variables)
+const numbers = computed(() => result.value?.draw?.draws[0]?.numbers)
 </script>
 
 <template>
   <div class="about">
-    <h1 class="text-3xl">About</h1>
-    <pre>{{ result || 'No result' }}</pre>
+    <!-- loading state -->
+    <div v-if="loading">Loading...</div>
+    <!-- error state -->
+    <div v-else-if="error">{{ error.message }}</div>
+
+    <main v-if="result">{{ numbers || 'No numbers today' }}</main>
   </div>
 </template>

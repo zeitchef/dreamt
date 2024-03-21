c7f586dc34dffc6c005a2b4bfe07a2208f45c2d6
Scott Voyles <zeitchef@gmail.com>
2024-03-21 10:17:50 +0100

    feat: add 404

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <main class="h-[calc(100vh-128px)] w-full flex flex-col justify-center items-center">
+    <img src="../assets/losing.avif" alt="Girl clutching to her losing tickets" class="rounded-lg">
+    <p>
+      Oh no! Better luck next time. Feel free to try your luck again!
+    </p>
+    <RouterLink to="/" class="text-blue-500"></RouterLink>
+  </main>
+</template>

e9d06f9634b33b604c432025073784d8391a5950
Scott Voyles <zeitchef@gmail.com>
2024-03-25 23:32:43 +0100

    fix: properly type Intl.DateTimeFormatOptions

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,8 @@
 export const formatDate = (date: Date) => {
-  const dateOptions = { year: 'numeric', month: 'short', day: 'numeric' }
+  const dateOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  } as Intl.DateTimeFormatOptions
   return new Intl.DateTimeFormat('en-US', dateOptions).format(date).replace(/,/g, '')
 }
